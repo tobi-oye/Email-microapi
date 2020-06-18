fc08393e3fef4e077dcdb7af41ba621af8c9e360
Aeesha <dolapo.toye@yahoo.com>
2020-06-18 22:41:28 +0200

    Added new schedlued mail sending and new error codes

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('sendmail', views.SendMail.as_view()),
     path('sendmailwithtemplate', views.SendMailWithTemplate.as_view()),
-    path('register', views.UserCreate.as_view(), name='account-create')
+    path('register', views.UserCreate.as_view(), name='account-create'),
+    path('sendscheduledmail', views.SendScheduledMail.as_view())
 ]

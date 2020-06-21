560546caa7ccc070bc5137e312e12d7fd88a8b96
tobi-oye <[tobiloba5000@gmail.com]>
2020-06-21 17:53:29 +0100

    email tracking service

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dell-g5\documents\python\hng_7.0\stage 3\email api new task\email-microapi\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
